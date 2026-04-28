3460c0b5a0f062374940f8c42d6f01c0313d662a
idreamfyrei <saumya.x3@gmail.com>
2026-04-29 01:03:05 +0530

    chore: update files

---
+++ index.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import app from "./src/app.js";
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
