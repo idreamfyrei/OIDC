9fc2e6c22fb8ae7167bedd87ad8af86a099c2dd7
idreamfyrei <saumya.x3@gmail.com>
2026-04-29 01:29:11 +0530

    feat: db migration

---
+++ drizzle/0000_cool_hellcat.sql
@@ -0,0 +1,48 @@
+CREATE TABLE "oauth_authorization_codes" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"code" text NOT NULL,
+	"user_id" uuid NOT NULL,
+	"client_id" varchar(255) NOT NULL,
+	"redirect_uri" text NOT NULL,
+	"scope" text NOT NULL,
+	"nonce" text,
+	"code_challenge" varchar(128) NOT NULL,
+	"code_challenge_method" varchar(10) NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "oauth_refresh_tokens" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"token" text NOT NULL,
+	"user_id" uuid NOT NULL,
+	"client_id" varchar(255) NOT NULL,
+	"scope" text NOT NULL,
+	"nonce" text,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"rotated_from_token_id" uuid
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"first_name" varchar(25),
+	"last_name" varchar(25),
+	"profile_image_url" text,
+	"email" varchar(322) NOT NULL,
+	"email_verified" boolean DEFAULT false NOT NULL,
+	"password" varchar(255),
+	"salt" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+ALTER TABLE "oauth_authorization_codes" ADD CONSTRAINT "oauth_authorization_codes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "oauth_refresh_tokens" ADD CONSTRAINT "oauth_refresh_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE UNIQUE INDEX "oauth_authorization_codes_code_unique_idx" ON "oauth_authorization_codes" USING btree ("code");--> statement-breakpoint
+CREATE INDEX "oauth_authorization_codes_user_id_idx" ON "oauth_authorization_codes" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "oauth_authorization_codes_expires_at_idx" ON "oauth_authorization_codes" USING btree ("expires_at");--> statement-breakpoint
+CREATE UNIQUE INDEX "oauth_refresh_tokens_token_unique_idx" ON "oauth_refresh_tokens" USING btree ("token");--> statement-breakpoint
+CREATE INDEX "oauth_refresh_tokens_user_id_idx" ON "oauth_refresh_tokens" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "oauth_refresh_tokens_expires_at_idx" ON "oauth_refresh_tokens" USING btree ("expires_at");--> statement-breakpoint
+CREATE UNIQUE INDEX "users_email_unique_idx" ON "users" USING btree ("email");
